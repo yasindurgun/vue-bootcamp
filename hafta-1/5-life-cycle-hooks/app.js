b9e6342e6a78a03e2bba0ead47aeb8133aad90ab
yasindurgun <yasin.durgun52@gmail.com>
2022-04-19 01:00:24 +0300

    life cycle hooks example

---
+++ hafta-1/5-life-cycle-hooks/app.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            title: "Test Başlığı",
+            itemList: [],
+        };
+    },
+    beforeCreate() {
+        console.log("beforeCreate çalıştı");
+    },
+    created() {
+        console.log("created çalıştı");
+        setTimeout(() => {
+            this.itemList = [1,2,3,4,5,6,7,8,9];
+        },2000);
+    },
+    beforeMount() {
+        console.log("beforeMount çalıştı");
+    },
+    mounted(){
+        console.log("mounted çalıştı");
+    },
+    beforeUpdate(){
+        console.log("beforeUpdate çalıştı");
+    },
+    updated(){
+        console.log("updated çalıştı");
+    },
+    beforeUnmount(){
+        console.log("beforeUnmount çalıştı");
+    },
+    unmounted(){
+        console.log("unmounted çalıştı");
+    },
+});
+
+app.mount("#app");
