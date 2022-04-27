8ad000ddad316e1c34e91d628743557dffe32036
yasindurgun <yasin.durgun52@gmail.com>
2022-04-27 15:44:27 +0300

    single file components example

---
+++ hafta-2/1-vue-cli/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<app-header/>
+<div class="container">
+<h3>İlk vue cli app</h3>
+    <p>paragraf</p>
+</div>
+<CounterItem></CounterItem>
+<CounterItem/>
+<CounterItem/>
+<CounterItem/>
+</template>
+
+<script>
+    import CounterItem from "@/components/CounterItem";
+    export default {
+        components : {
+            // CounterItem : CounterItem,
+            //key value aynıysa direkt yazılabilir.
+            CounterItem
+        }
+    }
+</script>
---
+++ hafta-2/1-vue-cli/src/components/appHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        Vue3 Bootcamp - Youtube
+    </header>
+</template>
+
+<style>
+    header{
+        background-color: rebeccapurple;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 50px;
+        border-bottom: 2px solid rgb(5,5,5);
+        position: absolute;
+        top: 0;
+        bottom: 0;
+    }
+</style>
